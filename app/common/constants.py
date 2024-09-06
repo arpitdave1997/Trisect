d8ebeff7be2187e3b20196b371ff2cf67653c35a
Arpit Dave <arpitdave321@gmail.com>
2024-09-06 22:20:07 +0530

    [dev] Integrating frontend with backend

--- app/common/constants.py
+++ app/common/constants.py
@@ -4,6 +4,8 @@ from app.common.enums import GameplayType, GameplayStatus
 ORIGINS = [
     "http://127.0.0.1:5501",
     "http://localhost:5501",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 BOT_USER_ID = 'BOT_USER'
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,7 +1,3 @@
-document.getElementById('playNow').addEventListener('click', function () {
-    document.getElementById('menu').style.display = 'none';
-    document.getElementById('game').style.display = 'block';
-});
 
 const cells = document.querySelectorAll('.cell');
 
@@ -33,27 +29,106 @@ cells.forEach(cell => {
 });
 
 var user_details = {};
+var session_details = {};
+var base_url = 'http://127.0.0.1:8000'
+var register_path = '/users/details'
+var update_path = '/users/activity/'
+var websocket_path = '/gameplay'
 
-function userLogin() {
-    const apiUrl = 'http://localhost:8000/get-ip';
-    fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        user_details = data
-    })
-    .catch(error => {
-        console.error('There was a problem with the registration request:', error);
+document.getElementById('play-now').addEventListener('click', async function () {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
+
+    await initiateGameplay();
+
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('game').style.display = 'block';
+});
+
+async function initiateGameplay(url) {
+    return new Promise((resolve, reject) => {
+        url = base_url.concat(websocket_path)
+
+        const socket = new WebSocket(url);
+        const initiateGameplayBody = JSON.stringify({
+            "case": "initiate_session",
+            "user_id": user_details.user_id
+        });
+
+        socket.addEventListener('open', function () {
+            socket.send(initiateGameplayBody);
+        });
+
+        socket.addEventListener('message', function (event) {
+            session_details = JSON.parse(event.data)[1]
+            resolve(event.data);
+        });
+
+        socket.addEventListener('error', function (error) {
+            console.error('WebSocket error: ', error);
+            reject(error);
+        });
     });
 }
 
-window.onload = userLogin;
+
+async function registerUser() {
+    const url = base_url.concat(register_path);
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+
+        user_details = data;
+        document.getElementById('menu').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
+
+        return user_details;
+    } catch (error) {
+        console.error('There was a problem with the registration request:', error);
+        throw error;
+    }
+}
+
+async function updateUserActivity(user_id) {
+    const url = base_url.concat(update_path, user_id)
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        return data.status;
+    } catch (error) {
+        console.error('There was a problem with the registration request:', error);
+        throw error;
+    }
+}
+
+async function onloadActions() {
+    while (true) {
+        if (!user_details.user_id) {
+            await registerUser()
+        } 
+        await new Promise(r => setTimeout(r, 5000));
+        await updateUserActivity(user_details.user_id)
+    }
+}
+
+window.onload = onloadActions;
