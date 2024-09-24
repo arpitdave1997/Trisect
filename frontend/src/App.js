419a26b94d315859616ffaa5ca18234f3d44cf1d
Arpit Dave <arpitdave321@gmail.com>
2024-09-25 00:46:58 +0530

    [dev] Reimplementation acc to new design

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Trisect from './Components/Trisect/trisect';
+
+function App() {
+  return (
+    <div>
+      <Trisect/>
+      
+    </div>
+  );
+}
+
+export default App;
--- run.py
+++
@@ -1,8 +0,0 @@
-from config import Config
-from app import createApp
-
-app = createApp()
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("run:app", port = Config.PORT, reload = True)
